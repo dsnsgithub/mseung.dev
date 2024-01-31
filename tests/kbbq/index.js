fb42adc3af92a5efdda27dd32d169344a66170d8
maxas10 <maxaseung@gmail.com>
2024-01-30 20:46:55 -0800

    birthdays update

--- tests/kbbq/index.js
+++ tests/kbbq/index.js
@@ -1,13 +1,18 @@
 const total = document.getElementById("total");
 const tax = document.getElementById("tax");
 const zipbox = document.getElementById("zipbox");
+const foodbox = document.getElementById("foodbox");
+const productcontainer = document.getElementById("productcontainer");
+var term;
 var taxstorage = 0;
 var tipstorage = 0;
 var subtotalstorage = 0;
 var zipcode;
+var nodes = [];
 
 var tempitem;
 var localCurrency = "$"
+var database;
 
 function change(input, destination, pretext) {
     if(document.getElementById(input).value >= 0 && document.getElementById(input).value != "") {
@@ -49,16 +54,41 @@ async function getSalesTax() {
 
 async function getGroceryPrices() {
     try {
-        await fetch("https://api.api-ninjas.com/v1/webscraper?url=https://cooklist.com/products/bakery/frozen-bread", {
-            method: 'GET',
-            headers: {'X-Api-Key': 'TkyIrYGNBO+wNc7GmAYt2w==OiLpDN4aV4VycmW9'},
+        await fetch("./db.json", {
+            method: "GET",
             contentType: 'application/json',
         })
-        .then(response => response.json().then(d => console.log(d)));
+        .then(data => data.json())
+        .then(k => database = k);
     } 
-    catch (error) {
-        console.error();
+    finally {
+        term = foodbox.value.toLowerCase();
+        productcontainer.innerHTML = ""
+        nodes = [];
+        if(term != "") {
+            for(let item in database) {
+                for(let subitem in database[item]) {
+                    for(let product in database[item][subitem]) {
+                        if(product.includes(term)) {
+                            nodes.push(product)
+                        }
+                    }
+                }
+            }
+        }
+
+        var order = 0;
+        nodes.sort(function(a, b){return a.length-b.length})
+
+        for(let node in nodes) {
+            var productCard = document.createElement("div");
+            var submitButton = document.createElement("button");
+            var productName = document.createElement("p");
+            var productDescription = document.createElement("p");
+            var productPrice = document.createElement("p");
+            productCard.innerHTML = nodes[node];
+            productcontainer.appendChild(productCard)
+
+        }
     }
 }
-
-getGroceryPrices()

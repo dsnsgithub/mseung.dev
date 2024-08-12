eda80d021d9105f7ecbd55044c8e58f188be92e0
maxas10 <maxaseung@gmail.com>
2024-08-11 22:30:08 -0700

    mseung.dev 1.3

--- script.js
+++ script.js
@@ -119,26 +119,26 @@
 // 	slide();	
 // }
 // // /*times opened--------- */
-// const timesOpened = document.getElementById("timesopened");
-// const firstOpened = document.getElementById("firstvisited");
-// if(localStorage.times) {
-//     localStorage.times = Number(localStorage.times) + 1;
-// 	timesOpened.innerHTML = localStorage.times;
-//     firstOpened.innerHTML = localStorage.first;
-// } else {
-//     localStorage.times = 1;
-// 	var currentTime = new Date();
-//     localStorage.first = "First visited on " + (Number(currentTime.getMonth()) + 1) + "/" + currentTime.getDate() + "/" + currentTime.getFullYear();
-// 	timesOpened.innerHTML = localStorage.times;
-//     firstOpened.innerHTML = localStorage.first;
-// }
+const timesOpened = document.getElementById("timesopened");
+const firstOpened = document.getElementById("firstvisited");
+if (localStorage.times) {
+    localStorage.times = Number(localStorage.times) + 1;
+    timesOpened.innerHTML = localStorage.times;
+    firstOpened.innerHTML = localStorage.first;
+} else {
+    localStorage.times = 1;
+    var currentTime = new Date();
+    localStorage.first = "First visited on " + (Number(currentTime.getMonth()) + 1) + "/" + currentTime.getDate() + "/" + currentTime.getFullYear();
+    timesOpened.innerHTML = localStorage.times;
+    firstOpened.innerHTML = localStorage.first;
+}
 // // /*times opened--------- */
 
 // // /*emoji launcher------ */
 // var angleXList = [];
 // var angleYList = [];
 // var emojiList = document.getElementsByClassName("emoji");
-// var emojis = ["sun.png"];
+// var emojis = [];
 // var emojiClient;
 // var moveInterval;
 // var createInterval;
@@ -215,7 +215,7 @@
 // /*emoji launcher------ */
 
 // var aList = document.querySelectorAll("a");
-// var date = new Date();
+var date = new Date();
 // if (date.getHours() >= 19 || date.getHours() <= 6) {
 // 	document.body.style.backgroundColor = "#070620";
 // 	document.body.style.color = "white";
@@ -280,17 +280,52 @@
 // 	emojis.push("present.png")
 // }
 
+const seasonBackground = document.getElementById("season-background");
+if (date.getMonth() == 9) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/halloween.jpg)'
+}
+if (date.getMonth() == 10 || date.getMonth() == 8) {
+        seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/autumntrees.jpg)'
+}
+
+if (date.getMonth() >= 5 && date.getMonth() <= 7) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/summer.jpg)'
+}
+if (date.getMonth() == 11 || date.getMonth() == 0) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/winter.webp)'
+}
+if (date.getMonth() == 1) {
+        seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/february.jpg)'
+}
+if (date.getMonth() == 1 && date.getDate() == 29) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/leapday.jpg)'
+}
+if (date.getMonth() == 3) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/spring.jpg)'
+}
+if (date.getMonth() == 4) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/cherryblossoms.webp)'
+}
+if (date.getMonth() == 2) {
+    seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/march.jpg)'
+}
+if ((date.getMonth() == 0 && date.getDate() == 1) || (date.getMonth() == 6 && date.getDate() == 4)) {
+seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/fireworks.jpg)'
+}
+if (date.getMonth() == 11 && date.getDate() == 25) {
+seasonBackground.style.backgroundImage = 'url(./pictures/main-site-images/christmas.jpg)'
+}
 
 async function getLocation() {
     if (navigator.geolocation) {
         const getCoords = async () => {
             const pos = await new Promise((resolve, reject) => {
-              navigator.geolocation.getCurrentPosition(resolve, reject);
+                navigator.geolocation.getCurrentPosition(resolve, reject);
             });
-        
+
             return {
-              lon: pos.coords.longitude,
-              lat: pos.coords.latitude,
+                lon: pos.coords.longitude,
+                lat: pos.coords.latitude,
             };
         };
 
@@ -300,38 +335,54 @@ async function getLocation() {
     }
 }
 
-async function fetchLocation(lat,lon) {
+async function fetchLocation(lat, lon) {
     const res = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=c95438ae35d06a4034d5edf752024f48&units=imperial');
     return await res.json();
 }
 
 const pictureFrame = document.getElementById("pictureframe");
 const loading = document.getElementById("loading")
+const weatherdesc = document.getElementById("weatherdesc");
+weatherdesc.innerHTML = "Weather Description: "
 async function changeSkyColor(data) {
+    var postQuery = [];
+    if (Math.random() <= 0.5) {
+        postQuery.push("%20aesthetic")
+    }
+    if (Math.random() <= 0.5) {
+        postQuery.push("%20nature")
+    }
+    if (Math.random() <= 0.5) {
+        postQuery.push("%20wallpaper")
+    }
+    postQuery = postQuery.join("")
     const date = new Date();
     const seconds = date.getTime() / 1000;
     var timeQuery = '';
     var weatherQuery = '';
-    if(seconds >= data.sys.sunrise && seconds <= data.sys.sunrise + 600) {
-        console.log(sec)
+    if (seconds >= data.sys.sunrise && seconds <= data.sys.sunrise + 600) {
         timeQuery = 'sunrise'
     } else if (seconds >= data.sys.sunset && seconds <= data.sys.sunset + 600) {
         timeQuery = 'sunset'
     } else if (seconds >= data.sys.sunrise + 600 && seconds <= data.sys.sunset) {
         timeQuery = 'sunny day'
     } else if (seconds <= data.sys.sunrise || seconds >= data.sys.sunset + 600) {
-        console.log(seconds, data.sys.sunset + 600)
-        timeQuery = 'dark%20night';
+        timeQuery = 'dark night';
     }
-    console.log(data)
-    console.log([timeQuery,weatherQuery])
-    weatherQuery = data.weather[0].description.split(' ').join('%20');
-    console.log(timeQuery,weatherQuery)
-    const res = await fetch('https://cors.dsns.dev/server.mseung.dev/?query=' + timeQuery + "%20" + weatherQuery + "%20nature" );
-    
-   /** @type {String[]} */
+    weatherQuery = data.weather[0].description;
+    weatherdesc.innerHTML = "Weather Description: " + timeQuery + ", " + weatherQuery;
+    weatherQuery = weatherQuery.split(' ').join('%20')
+    timeQuery = timeQuery.split(' ').join('%20');
+    const res = await fetch('https://cors.dsns.dev/server.mseung.dev/?query=' + timeQuery + "%20" + weatherQuery + postQuery);
+
+    /** @type {String[]} */
     const imgURLS = await res.json();
-    const cutURLS = imgURLS.slice(1,imgURLS.length-1)
+    return imgURLS
+}
+
+
+async function chooseRandomImage(imgURLS) {
+    const cutURLS = imgURLS.slice(1, imgURLS.length - 1)
     const randomNumber = Math.floor(Math.random() * cutURLS.length)
     const imageURL = cutURLS[randomNumber]
     pictureFrame.style.backgroundImage = `url(${imageURL})`
@@ -341,25 +392,32 @@ async function changeSkyColor(data) {
 async function run() {
     const coords = await getLocation()
     const data = await fetchLocation(coords["lat"], coords["lon"])
-    changeSkyColor(data);
+    const imgURLs = await changeSkyColor(data);
     setInterval(async () => {
-        await changeSkyColor(data)
-    }, 60000)
+        await chooseRandomImage(imgURLs)
+    }, 5000)
 }
 
-run();
+run()
 
 // offset
-
-const firstMainProject = document.getElementById('main-projects-container').children[0].getBoundingClientRect();
-const lastMainProject = document.getElementById('main-projects-container').children[document.getElementById('main-projects-container').children.length-1].getBoundingClientRect();
-const bodyDimensions = document.getElementById("other-stuff").getBoundingClientRect();
+const topics = document.getElementsByClassName("topic");
 function mainProjectsOffset() {
-
-    const topOffset = (bodyDimensions.height - firstMainProject.height)/2
-    const bottomOffset = (bodyDimensions.height - lastMainProject.height)/2
-    document.getElementById('main-projects-container').style.paddingTop = topOffset + "px"
-    document.getElementById('main-projects-container').children[0].style.marginBottom = topOffset + 'px'
-    document.getElementById('main-projects-container').style.paddingBottom = bottomOffset + "px"
+    const firstMainProject = document.getElementById('main-projects-container').children[0].getBoundingClientRect();
+    const lastMainProject = document.getElementById('main-projects-container').children[document.getElementById('main-projects-container').children.length - 1].getBoundingClientRect()
+    const bodyDimensions = seasonBackground.getBoundingClientRect();
+    const topOffset = (bodyDimensions.height - firstMainProject.height) / 2
+    const bottomOffset = (bodyDimensions.height - lastMainProject.height) / 2
+    document.getElementById('main-projects-container').style.paddingBottom = bottomOffset + "px";
+    for(let topic of topics) {
+        topic.style.marginTop = topOffset + 'px'
+    }
+    document.getElementById('times').style.left = (bodyDimensions.width - 1250) / 2 + 'px';
+    document.getElementById('times').style.top = bodyDimensions.height - parseInt(document.getElementById('times').offsetHeight) + 'px';
 }
-mainProjectsOffset()
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function() {
+        mainProjectsOffset()
+        window.addEventListener('resize', function() {mainProjectsOffset()})
+    }, 100)
+});

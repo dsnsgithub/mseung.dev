8e49e96429bc48dc6f996ef00a73d8eb5e7578c1
maxas10 <maxaseung@gmail.com>
2024-10-06 23:17:46 -0700

    add text

--- tests/testing/index.js
+++ tests/testing/index.js
@@ -1,5 +1,39 @@
-var numbers = document.getElementById("numbers").value
-function add() {
-    numbers.value = eval(numbers.value)
-    console.log(numbers)
+const introText = document.getElementById("introtext")
+passage = [
+    '...',
+    'Hello.',
+    'My name is Max Seung.',
+]
+
+var x = 0;
+var l = 0;
+var letterInterval;
+
+function startup() {
+	function addSentence(word) {
+        if(x < word.length) {
+            introText.innerHTML += word.charAt(x);
+            x++;
+        } else {
+            clearInterval(letterInterval)
+            setTimeout(function(){letterInterval = setInterval(function() {removeSentence()}, 100)}, 1000)
+        }
+	}
+
+	function removeSentence() {
+        if(introText.innerHTML.length > 0) {
+            introText.innerHTML = introText.innerHTML.slice(0, -1)
+            x--;
+        } else {
+            clearInterval(letterInterval)
+            if(l < passage.length-1) {
+                l++;
+                setTimeout(function(){letterInterval = setInterval(function() {addSentence(passage[l])}, 100)}, 1000)
+            }
+        }
+	}
+
+    letterInterval = setInterval(function() {addSentence(passage[l])}, 100)
 }
+
+startup();
---
+++ tests/text/index.js
@@ -0,0 +1,85 @@
+const IP = '10.3.3.222' /* This is the only server for now */
+const port = '5000'
+const username = document.getElementById("name")
+const message = document.getElementById("message");
+let socket;
+var special = false;
+var clients = false;
+const specialMessages = document.getElementsByClassName("special-text");
+async function connectToIP() {
+    if (username.value != '') {
+        document.getElementById("start-screen").style.display = "none";
+        document.getElementById("loading-screen").style.display = "flex";
+
+        socket = new WebSocket("ws://" + IP + ":" + port);
+
+        socket.addEventListener("open", () => {
+            document.getElementById("connected-status").innerHTML = 'Connected';
+            document.getElementById("connected-status").style.animation = "fade 1s ease-in-out 1 both";
+            document.getElementById("chat-screen").style.display = "flex";
+            setTimeout(function() {document.getElementById("loading-screen").style.display = "none";}, 1000)
+            socket.send(username.value)
+        })
+
+        socket.addEventListener("message", (event) => {
+            if(event.data == "special") {
+                special = true;
+                handleCreateSpecialText()
+            } else if (event.data == "normal") {
+            } else if (event.data == 'clients') {
+                clients=true;
+            } else {
+                if(clients) {
+                    document.getElementById("all-clients").innerHTML = '';
+                    for (let name of JSON.parse(event.data)) {
+                        const client = document.createElement("p");
+                        client.innerHTML = '- ' + name;
+                        document.getElementById("all-clients").appendChild(client);
+                    }
+                    clients = false;
+                } else {
+                    console.log(event.data)
+                    handleCreateMessage(event.data)   
+                }
+            }
+        })
+
+        socket.addEventListener("close", () => {
+            disconnect()
+            alert("The server has closed.")
+        })
+    } else {
+        alert("Please enter a username")
+    }
+}
+
+function sendMessage() {
+    socket.send(message.value)
+}
+
+function disconnect() {
+    socket.close()
+    document.getElementById("chat-screen").style.display = "none";
+    document.getElementById("start-screen").style.display = "flex";
+    document.getElementById("connected-status").innerHTML = 'Connecting...';
+    document.getElementById("connected-status").style.animation = "none";  
+    document.getElementById("chat").innerHTML = '';
+}
+
+function handleCreateMessage(data) {
+    const chatMessage = document.createElement("p");
+    chatMessage.innerHTML = data;
+    if (special && specialMessages.length > 0) {
+        specialMessages[specialMessages.length-1].appendChild(chatMessage)
+        special = false;
+    } else{
+        document.getElementById("chat").appendChild(chatMessage);
+    }
+}
+
+function handleCreateSpecialText() {
+    special = true;
+    const specialMessage = document.createElement("div");
+    specialMessage.classList.add("special-text");
+    document.getElementById("chat").appendChild(specialMessage);
+}
